5e702177ad34f2b2c80bb69b8bcc582053c6e469
theuncleofAlex <alex@yang2k.com>
2021-12-07 22:17:48 -0500

    basic navbar

--- src/Home.js
+++ src/Home.js
@@ -2,82 +2,22 @@ import './App.css';
 import React from 'react';
 import logo from './logo.svg';
 
+class Landing extends React.Component {
+    render() {
+        return (
+            <div className="landing">
+
+            </div>
+        );
+    }
+}
+
+
 class Home extends React.Component {
     render() {
         return (
             <div className="App">
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
-                Hello there<br/>
+            <Landing />
             </div>
 
         );
